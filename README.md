536e118c9a26f5cef28b6641dcfe82eff150aae9
Jack Lu <18733796+dangkychua@users.noreply.github.com>
2024-04-14 10:25:36 +0700

    first commit

---
+++ README.md
@@ -0,0 +1,40 @@
+# ServiceNow PDI Wakeup 24/7 ![VERSION](https://img.shields.io/badge/version-1.0-green.svg)
+
+![PyPI - Python Version](https://img.shields.io/pypi/pyversions/Django.svg) ![MIT](https://img.shields.io/badge/license-MIT-blue)
+
+You're tired of having to wake up PDI every week. If yes, this is for you.
+Kick ServiceNow's ass to wake up PDI 24/7. 
+
+## Requirements
+
+- python (3.10+)
+- chrome browser (latest version)
+- window 10
+
+## Install Requirements
+    pip install -r requirements.txt
+
+## How it works
+
+1. Make sure you have already installed *Requirements*.
+2. Clone this repo.
+3. Install the remaining requirements with `pip install -r requirements.txt`
+4. Create config file `.env` at root folder as *Configuration* below
+4. Run app as `py j.py`
+
+## Configuration
+
+|     Name     	|          Description         	|                       Example                      	|
+|:------------:	|:----------------------------:	|:--------------------------------------------------:	|
+|    SILENT    	|    Run app as silent mode    	| True: Run as silent mode False: Run as normal mode 	|
+| INSTANCE_URL 	|       PDI Instance URL       	|         https://your-PDI.service-now.com         	|
+|  J_USERNAME  	| PDI admin account (username) 	|                        admin                       	|
+|  J_PASSWORD  	| PDI admin account (password) 	|                     P@ssword123                    	|
+|  A_USERNAME  	|   Your Dev account (email)   	|                  my@email.com                 	|
+|  A_PASSWORD  	|  Your Dev account (password) 	|                     P@ssword123                    	|
+
+### Donation
+
+If you love my apps, give me a buck bros!
+
+[![Paypal](https://img.shields.io/badge/donate-paypal-blue)](https://paypal.me/calu276)
---
+++ j.py
@@ -0,0 +1,227 @@
+import time
+import os
+import sys
+from dotenv import load_dotenv
+from cryptography.fernet import Fernet
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.common.exceptions import *
+
+
+# constant
+timer = 2
+config = os.path.join(os.getcwd(), ".env")
+if os.path.isfile(config):
+    load_dotenv(config)
+else:
+    print(">>> Missing Config File")
+    for i in range(5, -1, -1):
+        print(f"Auto close after {i} seconds....", end="\r", flush=True)
+        time.sleep(1)
+    sys.exit(0)
+
+
+CALU = os.getenv("CALU") == "True"
+SILENT = os.getenv("SILENT") == "True"
+key = os.getenv("K").encode()
+fernet = Fernet(key)
+DEBUG = False
+INSTANCE_URL = os.getenv("INSTANCE_URL")
+J_USERNAME = fernet.decrypt(os.getenv("J_USERNAME")).decode(
+) if CALU else os.getenv("J_USERNAME")
+J_PASSWORD = fernet.decrypt(os.getenv("J_PASSWORD")).decode(
+) if CALU else os.getenv("J_PASSWORD")
+A_USERNAME = fernet.decrypt(os.getenv("A_USERNAME")).decode(
+) if CALU else os.getenv("A_USERNAME")
+A_PASSWORD = fernet.decrypt(os.getenv("A_PASSWORD")).decode(
+) if CALU else os.getenv("A_PASSWORD")
+
+if DEBUG:
+    print(CALU)
+    print("-----------------------------")
+    print(f"SLIENT MODE: {SILENT}")
+    print(f"INSTANCE URL: {INSTANCE_URL}")
+    print(f"J_USERNAME: {J_USERNAME}")
+    print(f"J_PASSWORD: {J_PASSWORD}")
+    print(f"A_USERNAME: {A_USERNAME}")
+    print(f"A_PASSWORD: {A_PASSWORD}")
+
+# init
+chrome_options = webdriver.ChromeOptions()
+chrome_options.add_argument("--incognito")
+if SILENT:
+    print("*****Start with silent mode")
+    chrome_options.add_argument("--headless")
+else:
+    print("*****Start with normal mode")
+    chrome_options.add_argument("--start-maximized")
+    chrome_options.add_experimental_option("detach", True)
+driver = webdriver.Chrome(options=chrome_options)
+wait = WebDriverWait(driver, 10)
+if not DEBUG:
+    driver.get(INSTANCE_URL)
+if DEBUG:
+    driver.close()
+
+
+def login():
+    print("--------- START LOGIN PROCESS")
+    log("Start Authenticate...")
+    time.sleep(timer)
+    username_field = driver.find_element(By.ID, "user_name")
+    password_field = driver.find_element(By.ID, "user_password")
+    submit_btn = driver.find_element(By.ID, "sysverb_login")
+    username_field.send_keys(J_USERNAME)
+    password_field.send_keys(J_PASSWORD)
+    submit_btn.click()
+    time.sleep(timer)
+    try:
+        ele = WebDriverWait(driver, 5).until(EC.presence_of_element_located(
+            (By.XPATH, "//*[@id='output_messages']/div/div/div")))
+        if ele.text == "User name or password invalid":
+            log("Login failed!")
+            log("Wrong username/password")
+            return False
+    except Exception:
+        pass
+
+    wait.until(EC.url_contains(INSTANCE_URL +
+               "/now/nav/ui/classic/params/target/ui_page.do"))
+    log("Login successful")
+    return True
+
+
+def action():
+    print("--------- START ACTION PROCESS")
+    log("Find Script-background menu")
+    time.sleep(timer)
+    ele = wait.until(EC.visibility_of(driver.execute_script(
+        "return document.querySelector('body > macroponent-f51912f4c700201072b211d4d8c26010').shadowRoot.querySelector('div > sn-canvas-appshell-root > sn-canvas-appshell-layout > sn-polaris-layout').shadowRoot.querySelector('div.sn-polaris-layout.polaris-enabled > div.layout-main > div.header-bar > sn-polaris-header').shadowRoot.querySelector('#d6e462a5c3533010cbd77096e940dd8c');")))
+    ele.click()
+    time.sleep(timer)
+    # check admin role
+    try:
+        ele = wait.until(EC.visibility_of(driver.execute_script(
+            "return document.querySelector('body > macroponent-f51912f4c700201072b211d4d8c26010').shadowRoot.querySelector('div > sn-canvas-appshell-root > sn-canvas-appshell-layout > sn-polaris-layout').shadowRoot.querySelector('div.sn-polaris-layout.polaris-enabled > div.layout-main > div.header-bar > sn-polaris-header').shadowRoot.querySelector('nav > div > sn-polaris-menu:nth-child(1)').shadowRoot.querySelector('#filter');")))
+        ele.click()
+        ele.send_keys("Scripts - Background")
+        time.sleep(timer)
+        wait.until(EC.visibility_of(driver.execute_script(
+            "return document.querySelector('body > macroponent-f51912f4c700201072b211d4d8c26010').shadowRoot.querySelector('div > sn-canvas-appshell-root > sn-canvas-appshell-layout > sn-polaris-layout').shadowRoot.querySelector('div.sn-polaris-layout.polaris-enabled > div.layout-main > div.header-bar > sn-polaris-header').shadowRoot.querySelector('nav > div > sn-polaris-menu:nth-child(1)').shadowRoot.querySelector('nav > div.sn-polaris-nav.d6e462a5c3533010cbd77096e940dd8c.can-animate > div.super-filter-container.all-results-open > div.all-results-section.section-open.results-section > div > div.sn-polaris-tab-content.-left.is-visible.can-animate > div > sn-collapsible-list').shadowRoot.querySelector('a[data-id=\"5e24af15c0a80a940121f06c64cf5982\"] > span > span.label > mark');")))
+
+    except Exception:
+        log("User has not admin role")
+        return False
+
+    driver.get(
+        INSTANCE_URL+"/now/nav/ui/classic/params/target/sys.scripts.modern.do")
+    log("Waiting navigate to Scripts - Background page....")
+    wait.until(EC.url_to_be(
+        INSTANCE_URL+"/now/nav/ui/classic/params/target/sys.scripts.modern.do"))
+    time.sleep(timer)
+    WebDriverWait(driver, 60).until(EC.frame_to_be_available_and_switch_to_it(driver.execute_script(
+        "return document.querySelector('body > macroponent-f51912f4c700201072b211d4d8c26010').shadowRoot.querySelector('#gsft_main');")))
+    log("Scripts - Background page Loaded!")
+    try:
+        time.sleep(timer)
+        driver.execute_script(
+            "changeJsEditorPreference_script_editor('false');")
+        wait.until(EC.alert_is_present())
+        driver.switch_to.alert.accept()
+    except Exception:
+        pass
+    time.sleep(timer)
+    driver.execute_script(
+        "document.querySelector('#script').value = 'gs.info(gs.now());'")
+    driver.execute_script("document.querySelector('#MANDATORY').click();")
+    log("Executing script....")
+    time.sleep(timer)
+    wait.until(EC.url_to_be(
+        INSTANCE_URL+"/now/nav/ui/classic/params/target/sys.scripts.do"))
+    # time.sleep(timer)
+    ele = driver.execute_script("return document.querySelector('body > pre').innerText;")
+    log(ele)
+    log("Execute script successful!")
+
+    return True
+
+
+def log(msg):
+    print(">>> ", msg)
+
+
+def wakeup():
+    print("--------- START WAKEUP PROCESS")
+    # waiting redirect to dev page
+    wait.until(EC.url_to_be(
+        "https://developer.servicenow.com/dev.do#!/home?wu=true"))
+    log("Navigate to DEV page")
+    log("Waiting DEV page loaded.....")
+    ele = wait.until(EC.visibility_of(driver.execute_script(
+        "return document.querySelector('body > dps-app').shadowRoot.querySelector('div > header > dps-navigation-header').shadowRoot.querySelector('header > div > div.dps-navigation-header-utility > ul > li:nth-child(2) > dps-login').shadowRoot.querySelector('div > dps-button').shadowRoot.querySelector('button')")))
+    log("DEV page loaded!")
+    ele.click()
+    # waiting redirect to login page
+    log("Navigate to LOGIN page")
+    log("Waiting LOGIN page loaded.....")
+    wait.until(EC.url_to_be(
+        "https://signon.service-now.com/x_snc_sso_auth.do?pageId=username"))
+    wait.until(EC.presence_of_all_elements_located((By.ID, "email")))
+    log("LOGIN page loaded!")
+    # Fulfilment authenticate
+    log("Get Email field")
+    ele = driver.find_element(By.ID, "email")
+    ele.click()
+    log("Enter Email")
+    ele.send_keys(A_USERNAME)
+    ele = driver.find_element(By.ID, "username_submit_button")
+    ele.click()
+    log("Go to next step")
+    wait.until(EC.presence_of_all_elements_located((By.ID, "password")))
+    log("Get Password field")
+    ele = driver.find_element(By.ID, "password")
+    ele.click()
+
+    log("Enter Password")
+    ele.send_keys(A_PASSWORD)
+    ele = driver.find_element(By.ID, "password_submit_button")
+    ele.click()
+    log("Start Authenticate...")
+    time.sleep(timer)
+
+    # Waiting instance wakeup
+    log("Waiting instance wakeup.....")
+    wait.until(EC.url_to_be(
+        "https://developer.servicenow.com/dev.do#!/home"))
+    time.sleep(timer)
+    ele = WebDriverWait(driver, 180).until(EC.element_to_be_clickable(driver.execute_script(
+        "return document.querySelector('body > dps-app').shadowRoot.querySelector('div > main > dps-home-auth-quebec').shadowRoot.querySelector('div > section:nth-child(1) > div > dps-page-header > div:nth-child(1) > button');")))
+    log("instance has been wake up!!!")
+    return True
+
+
+def main():
+    try:
+        if driver.title == "Instance Hibernating page":
+            log("Instance has been hibernated!")
+            wakeup()
+        else:
+            log("Instance still alive!")
+            if login():
+                action()
+    except Exception as e:
+        print("------------------has been error")
+        print(e)
+
+    for i in range(5, -1, -1):
+        print(f"Auto close after {i} seconds....", end="\r", flush=True)
+        time.sleep(1)
+    driver.close()
+    sys.exit(0)
+
+
+if __name__ == "__main__":
+    if not DEBUG:
+        main()
